9d0ae1c806da3553cc7abc38a9f7c54aa555657a
Bernardo de Moura Medeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-02-05 16:32:45 -0300

    Update main.asm

--- Jogo/main.asm
+++ Jogo/main.asm
@@ -1,18 +1,24 @@
 .text
 main:
-    # cenario da tela
+    # Cenário da tela
     jal criarCenario
     
-    # copia do cenario
+    # Cópia do cenário
     addi $4, $0, 32768
     jal criarCenario
     
+    # Cria o NPC
     addi $4, $0, 0
     jal criarNpc
     
-  
+    # Cria o player
     jal criarPlayer
-forMovenpc:
+
+    # Inicializa a posição do player
+    addi $25, $0, 0  # $25 armazena a posição do player
+
+loopPrincipal:
+    # Movimentação do NPC (função existente)
     add $4, $25, 30632
     jal forRestaurar
     
@@ -20,32 +26,30 @@ forMovenpc:
     add $4, $0, $25
     jal criarNpc
     
-       jal timer
-    j forMovenpc
-    
-    
-    addi $25, $25, -4
-   
     jal timer
-    j forMovenpc
-    
-forMoveplayer:
-	add $4, $25, 30220
-	jal criarPlayer
-    
- 
-timer: sw $16, 0($29)
-       addi $29, $29, -4
-       addi $16, $0, 50000
-forT:  beq $16, $0, fimT
-       nop
-       nop
-       addi $16, $16, -1      
-       j forT                  
-fimT:  addi $29, $29, 4                                                    
-       lw $16, 0($29)          
-       jr $31
-       
-fim:	
-       addi $2, $0, 10
-	syscall
+
+    # Movimentação do player (nova função)
+    jal forMovePlayer
+
+
+    j loopPrincipal
+
+# Função de delay 
+timer:
+    sw $16, 0($29)
+    addi $29, $29, -4
+    addi $16, $0, 500000
+forT:
+    beq $16, $0, fimT
+    nop
+    nop
+    addi $16, $16, -1      
+    j forT                  
+fimT:
+    addi $29, $29, 4                                                    
+    lw $16, 0($29)          
+    jr $31
+
+fim:
+    addi $2, $0, 10
+    syscall
