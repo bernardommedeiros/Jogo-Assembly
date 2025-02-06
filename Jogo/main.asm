60b0b35c061feac67198467f9fe9e77bc11489d4
Bernardo de Moura Medeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-02-06 11:53:58 -0300

    Update main.asm

--- Jogo/main.asm
+++ Jogo/main.asm
@@ -1,24 +1,24 @@
+#loopPlayer:
+ #   add $4, $25, 30632
+ #   jal forRestaurar2
+    
+  #  addi $25, $25, 4 #beq esq
+   # add $4, $0, $25
+   # jal criarPlayer
+    
 .text
 main:
-    # CenÃ¡rio da tela
+    # cenario da tela
     jal criarCenario
     
-    # CÃ³pia do cenÃ¡rio
+    # copia do cenario
     addi $4, $0, 32768
     jal criarCenario
     
-    # Cria o NPC
     addi $4, $0, 0
     jal criarNpc
     
-    # Cria o player
-    jal criarPlayer
-
-    # Inicializa a posiÃ§Ã£o do player
-    addi $25, $0, 0  # $25 armazena a posiÃ§Ã£o do player
-
-loopNpc:
-    # MovimentaÃ§Ã£o do NPC (funÃ§Ã£o existente)
+forLoop:
     add $4, $25, 30632
     jal forRestaurar
     
@@ -26,44 +26,24 @@ loopNpc:
     add $4, $0, $25
     jal criarNpc
     
-    jal timer
-
-    # MovimentaÃ§Ã£o do player (nova funÃ§Ã£o)
+       jal timer
+    j forLoop
+    
+    
+    
  
-    j loopNpc
-    
-loopPlayer:
-    add $4, $25, 30632
-    jal forRestaurar2
-    
-    addi $25, $25, 4 #beq esq
-    add $4, $0, $25
-    jal criarPlayer
-    
-    jal timer
-    
-    j loopPlayer
-    
-	
-               
-    
-
-# FunÃ§Ã£o de delay 
-timer:
-    sw $16, 0($29)
-    addi $29, $29, -4
-    addi $16, $0, 500000
-forT:
-    beq $16, $0, fimT
-    nop
-    nop
-    addi $16, $16, -1      
-    j forT                  
-fimT:
-    addi $29, $29, 4                                                    
-    lw $16, 0($29)          
-    jr $31
-
-fim:
-    addi $2, $0, 10
-    syscall
+timer: sw $16, 0($29)
+       addi $29, $29, -4
+       addi $16, $0, 50000
+forT:  beq $16, $0, fimT
+       nop
+       nop
+       addi $16, $16, -1      
+       j forT                  
+fimT:  addi $29, $29, 4                                                    
+       lw $16, 0($29)          
+       jr $31
+       
+fim:	
+       addi $2, $0, 10
+	syscall
