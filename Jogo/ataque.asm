234fd4f8df206c3d92e4b5c5676d43521f0e7f34
Bernardo de Moura Medeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-02-10 11:02:18 -0300

    Update ataque.asm

--- Jogo/ataque.asm
+++ Jogo/ataque.asm
@@ -1,8 +1,8 @@
-.globl forTiro
+.globl forTiro, moveTiro
 
 forTiro:
 
-    sw $31, 0($sp)          # Salva o endereço de retorno
+    sw $31, 0($sp)          # Salva o endereÃ§o de retorno
     addi $sp, $sp, -4
     
     sw $8, 0($sp)           # Salva $9
@@ -11,7 +11,7 @@ forTiro:
     sw $9, 0($sp)          # Salva $10
     addi $sp, $sp, -4
     
-    sw $10, 0($sp)          # Salva o endereço de retorno
+    sw $10, 0($sp)          # Salva o endereÃ§o de retorno
     addi $sp, $sp, -4
     
     sw $11, 0($sp)           # Salva $9
@@ -23,20 +23,21 @@ forTiro:
     sw $13, 0($sp)           # Salva $9
     addi $sp, $sp, -4
     
-     
-   lui $9, 0xffff
-   lw $10, 0($9) #verifica se houve mudanca no buffer
-   beq $10, $0, fimTiro
+    sw $14, 0($sp)           # Salva $9
+    addi $sp, $sp, -4
+    
+    sw $15, 0($sp)           # Salva $9
+    addi $sp, $sp, -4
+    
+    
+    beq $20, $0, espacoPressionado
+    bgt $20, $0, moveTiro #se 20 for maior que 0
    
-   lw $10, 4($9)
-   
-   li $8, 32 #ascii da barra de espaco
-   beq $8, $10, espacoPressionado
-   
-   j moveTiro
+   j fimTiro
    
 espacoPressionado:
   beq $20, $0, criarTiro
+  bgt $20, $0, moveTiro
   j fimTiro
    
 criarTiro:
@@ -53,8 +54,10 @@ criarTiro:
     
 moveTiro:
    jal forTiroNpc
+ 
    bne $3, $0, desfazTiro
    
+  
    lui $12, 0x1001
    add $12, $12, 32768
    add $12, $12, $20
@@ -66,14 +69,14 @@ moveTiro:
    add $14, $14, $20 # att posicao do tiro
    sw $10, 0($14)
    
-   addi $14, $14, 4
+   addi $14, $14, 12
    
    ori $15, $0, 0x000000
    sw $15, 0($14)
    
-   addi $20, $20, 4
+   addi $20, $20, 12
    
-   j fimTiro
+   j fimTiro2
 
 
 desfazTiro:
@@ -87,25 +90,61 @@ desfazTiro:
    
 fimTiro:
 
-   sw $31, 0($sp)          # Salva o endereço de retorno
-    addi $sp, $sp, -4
+    addi $sp, $sp, 4
+    lw $15, 0($sp)
     
-    sw $13, 0($sp)           # Salva $9
-    addi $sp, $sp, -4
+    addi $sp, $sp, 4
+    lw $14, 0($sp)
     
-    sw $12, 0($sp)           # Salva $9
-    addi $sp, $sp, -4
+    addi $sp, $sp, 4
+    lw $13, 0($sp)
     
-    sw $11, 0($sp)           # Salva $9
-    addi $sp, $sp, -4
+    addi $sp, $sp, 4
+    lw $12, 0($sp)
     
-    sw $10, 0($sp)          # Salva $10
-    addi $sp, $sp, -4
+    addi $sp, $sp, 4
+    lw $11, 0($sp)
     
-    sw $9, 0($sp)          # Salva o endereço de retorno
-    addi $sp, $sp, -4
+    addi $sp, $sp, 4
+    lw $10, 0($sp)
     
-    sw $8, 0($sp)           # Salva $9
-    addi $sp, $sp, -4
+    addi $sp, $sp, 4
+    lw $9, 0($sp)
     
-   jr $31
+    addi $sp, $sp, 4
+    lw $8, 0($sp)
+    
+    addi $sp, $sp, 4
+    lw $31, 0($sp)
+    j posTiro
+
+
+fimTiro2:
+
+    addi $sp, $sp, 4
+    lw $15, 0($sp)
+    
+    addi $sp, $sp, 4
+    lw $14, 0($sp)
+    
+    addi $sp, $sp, 4
+    lw $13, 0($sp)
+    
+    addi $sp, $sp, 4
+    lw $12, 0($sp)
+    
+    addi $sp, $sp, 4
+    lw $11, 0($sp)
+    
+    addi $sp, $sp, 4
+    lw $10, 0($sp)
+    
+    addi $sp, $sp, 4
+    lw $9, 0($sp)
+    
+    addi $sp, $sp, 4
+    lw $8, 0($sp)
+    
+    addi $sp, $sp, 4
+    lw $31, 0($sp)
+    j posMoveTiro
